b8bd22552fc84b502118a9c37cb1b97d78db04cc
Ben Parkes <benjamin.b.parkes@googlemail.com>
2020-07-06 16:59:55 +0100

    P0342 nav bar and header update

--- wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
@@ -17,6 +17,7 @@
 		<div class="site-branding-text">
 			<?php if ( is_front_page() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2>TEST</h2>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>

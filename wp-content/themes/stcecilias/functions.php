da763a6bed646179d87f01ce2dc2913a5ad5a9c2
Ben Parkes <benjamin.b.parkes@googlemail.com>
2020-12-16 17:21:20 +0000

    StCs - Audio play/pause buttons added

--- wp-content/themes/stcecilias/functions.php
+++ wp-content/themes/stcecilias/functions.php
@@ -13,13 +13,3 @@
             wp_get_theme()->get('Version')                  // load version meta
         );
     }
-    
-    /*function play() {
-        document.getElementByClass("img.wp-image-2636").onclick(document.getElementById("anon-harpsi").play());
-    }*/
-    /*function wpb_custom_new_menu() {
-        register_nav_menu('st-cecilias-menu',__( 'St Cecilias Menu' ));
-    }
-    add_action( 'init', 'wpb_custom_new_menu' );*/
-
-    
